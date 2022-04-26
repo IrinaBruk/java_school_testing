d2526564569550b4a1b9ef6d7051e03a0d8107a4
Irina Brukva <iabrukva@gmail.com>
2022-04-26 14:59:06 +0300

    study functions

--- sandbox/src/main/java/my/pkg/MyFirstProgram.java
+++ sandbox/src/main/java/my/pkg/MyFirstProgram.java
@@ -2,7 +2,30 @@ package my.pkg;
 
 public class MyFirstProgram {
 
- public static void main(String[] args) {
-   System.out.println("Hello, Yura!");
-  }
+    public static void main(String[] args) {
+        hello("Yuriy");
+        hello("Yura");
+        hello("Yuriy G");
+
+        double l = 5;
+        System.out.println("Ploshad' kvadrata so storonoy " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Ploshad' pryamougolnika so storonami " + a + " , " + b + " = " + area(a, b));
+    }
+
+
+    public static void hello(String somebody) {
+
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
 }
